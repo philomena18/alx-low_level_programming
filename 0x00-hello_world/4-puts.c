33cf71ab6555d8132698aa62a2f5bd3da7571e32
philomena18 <phillsbett92@gmail.com>
2023-04-20 17:09:18 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C program that prints a line  followed by a new line.
+ * Return: 0(Sucess)
+ */
+int main(void)
+{
+puts("/"Programming is like building a multilingual puzzle"/");
+return (0);
+}
