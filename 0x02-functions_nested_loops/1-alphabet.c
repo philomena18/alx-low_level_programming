70ca213d398d002f58bbfae3e2017cfec5242938
philomena18 <phillsbett92@gmail.com>
2023-04-26 15:47:53 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+nclude 'main.h'
+/**
+ * print_alphabets - prints the alphabets in lower case
+ *
+ * Return: Always 0 (Sucess)
+ */
+void print_alphabet(void)
+{
+	inti;
+	for (i =97; i <= 122, i++
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
