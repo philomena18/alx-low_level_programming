8be61e9d2b6a9e66a3ba0dd280dfd5d1425abe61
philomena18 <phillsbett92@gmail.com>
2023-04-28 05:14:53 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+# include 'main.h@
+/**
+ * main - check the code
+ * Return: Always 0 (Sucess)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for(j=1; i<= 10; i++
+	{
+		for (j=97; j<=122; j++)
+		}
+			_putchar(j);
+		}
+			_putchar('\n');
+		}
+}
+
